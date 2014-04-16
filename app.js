921f35498951b6ab869e223925ed403bc0b5e3d8
Niv Sardi <xaiki@evilgiggle.com>
2014-04-16 02:45:35 -0300

    make it work
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- app.js
+++ app.js
@@ -1,44 +1,69 @@
 var express = require('express'),
+    morgan  = require('morgan'),
+    busboy = require('connect-busboy'),
     fs = require('fs'),
     sleep = require('sleep'),
-    ImageTypes = require('./ImageTypes');
+    ImageTypes = require('./ImageTypes'),
+    multiparty = require('multiparty')
+, util = require('util')
+, moment = require('moment');
 
 var app = express(),
     PORT = 3000;
 
-app.configure(function() {
-    app.use(express.static('public'))
-    app.use(express.bodyParser());
-    app.set('view engine', 'dot' );
-    app.engine('dot', require('express-dot').__express );
+app.use(express.static('public'))
+app.use(morgan('dev'));
+app.set('view engine', 'dot' );
+app.engine('dot', require('express-dot').__express );
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
 });
 
 
-app.get('/', function(req, res) {
-    res.render('form', { layout: false });
-});
-
-app.post('/upload', function(req, res){
-    saveFile(req.files.imageData.path, ImageTypes[req.files.imageData.type]);
-
-    sleep.sleep(4);
-
-    res.setHeader('Content-Type', 'text/plain');
-    res.render('success', { layout: false});
-});
+app.route('/')
+    .get(function(req, res, next) {
+        res.render('form', { layout: false });
+    });
 
 
-function saveFile(path, extension) {
-    var fileData = fs.readFileSync(path),
-        savePath = getSavePath(extension);
+app.route('/upload')
+    .post(handleUpload);
+
+app.route('/upload/:user')
+    .post(handleUpload);
+
+app.route('//upload/:user')
+    .post(handleUpload);
+
+function handleUpload(req, res, next){
+    var form = new multiparty.Form();
+
+    form.parse(req, function(err, fields, files) {
+        files.file.forEach (function (file) {
+            saveFile(file, req.params.user);
+        });
+//        sleep.sleep(4);
+
+        res.setHeader('Content-Type', 'text/plain');
+  //      res.render('success', { layout: false});
+        res.end(util.inspect({fields: fields, files: files}));
+    });
+
+}
+
+function saveFile(file, name) {
+    var fileData = fs.readFileSync(file.path),
+        savePath = getSavePath(file, name);
 
     console.log("Saving in " + savePath);
     fs.writeFileSync(savePath, fileData);
     console.log("...done.");
 }
 
-function getSavePath(extension) {
-    return (__dirname + '/uploads/' + new Date().getTime() + '.' + extension);
+function getSavePath(file, name) {
+    return (__dirname + '/uploads/' + name + '-' + moment().format("YYYY-MM-DD-HH-mm") + '-' + file.originalFilename);
 }
 
 app.listen(PORT);
